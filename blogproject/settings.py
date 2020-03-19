2ec7b0be96c82b534058acc3aac03bfd29515a92
syu89 <yu.suo@foxmail.com>
2020-03-19 10:47:17 +0800

    rectify the charater

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    ‘blog', #注册blog应用
+    'blog', #注册blog应用
 ]
 
 MIDDLEWARE = [
